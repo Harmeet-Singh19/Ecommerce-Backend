2446bb1828237bd561dd700efc97eed6968d7fcd
Harmeet <savgun2001@gmail.com>
2020-11-18 14:14:32 +0530

    Enum change

--- Utils/enums.js
+++ Utils/enums.js
@@ -1,5 +1,5 @@
 module.exports={
     yearEnum:[1,2,3,4],
     subjectEnum:["maths","english","physics","economics","chemistry"],
-    courseEnum:["Bsc","Bscom","Engg"]
+    courseEnum:["Bsc","Bscom","Engg","Bcom"]
 }
