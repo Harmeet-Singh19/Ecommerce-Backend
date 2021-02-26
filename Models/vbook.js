df190cff33d539c2b216ad4b7847d34029a30ba8
vinay <vinaydahiya04@gmail.com>
2021-02-26 22:37:17 +0530

    bug fix

--- Models/vbook.js
+++ Models/vbook.js
@@ -3,35 +3,37 @@ const Schema = mongoose.Schema;
 //var uniqueValidator = require('mongoose-unique-validator');
 
 const bookSchema = mongoose.Schema({
-    //required fields
+  //required fields
 
-    name: {type:String,required:true},
-    image: {type:
-    [
-      {
-        type: String, default: null
-      }
-    ] },
-    description: { type: String, required: true },
-    weight: {type:Number}, 
-    edition:{type:Number},
-    author: {type:String},
-    publisher:{type:String},
-    seller: {type:Schema.Types.ObjectId,ref:'admin'},
+  name: { type: String, required: true },
+  image: {
+    type:
+      [
+        {
+          type: String, default: null
+        }
+      ]
+  },
+  description: { type: String, required: true },
+  weight: { type: Number },
+  edition: { type: Number },
+  author: { type: String },
+  publisher: { type: String },
+  seller: { type: Schema.Types.ObjectId, ref: 'admin' },
 
-    countInStock: {
-        type: Number,
-        required: true,
-        default: 0,
-      },
-    //filters
-    price: {type:Number, required: true},
-    year:{type:Number,enum:[1,2,3,4]},
-    subject:{type:String,enum:["maths","english","physics","economics","chemistry",]},
-    course:{type:String,enum:["Bsc","Bscom","Engg","Bcom"]},
-    hand:{type:Number,enum:[1,2]},
-    uploadAt:{type:Date}
-    
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  //filters
+  price: { type: Number, required: true },
+  year: { type: Number, enum: [1, 2, 3, 4] },
+  subject: { type: String, required: true },
+  course: { type: String, enum: ["Bms", "BcomH", "BcomP", "BaHEco", "BaHEng", "BaHPsy", "Shivdas", "BscHSta", "BscHMat"] },
+  hand: { type: Number, enum: [1, 2] },
+  uploadAt: { type: Date }
+
 });
 
 //Schema.plugin(uniqueValidator);
